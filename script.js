64deee74d816d55a50157e6a2fe4d4285fe053fd
MarioSilva <calebemp33@gmail.com>
2026-09-15 20:52:24 -0300

    Overlay para main, enquanto menu ativo

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let menu = document.getElementById('menu')
 let dropdown = document.querySelector('div.menu-fechado')
+let overlay = document.querySelector('.overlay')
+
 menu.addEventListener('click', (e) =>{
     dropdown.classList.toggle('menu-aberto')
     if (dropdown.classList.contains('menu-aberto')) {
         menu.setAttribute('src', 'icons/icon-close.svg')
+        overlay.classList.add('over-ativo')
     }else{
         menu.setAttribute('src', 'icons/icon-menu.svg')
+        overlay.classList.remove('over-ativo')
         
     }
 })
