8ed5440f2baa4a5aa45ddaad4ec7df0c7f45cf58
MarioSilva <calebemp33@gmail.com>
2026-09-18 12:06:05 -0300

    Navegação do menu pelo teclado

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 let menu = document.getElementById('menu')
 let dropdown = document.querySelector('div.menu-fechado')
 let overlay = document.querySelector('.overlay')
-
+// Abre e fecha o menu
 menu.addEventListener('click', (e) =>{
-    dropdown.classList.toggle('menu-aberto')
-    if (dropdown.classList.contains('menu-aberto')) {
-        menu.setAttribute('src', 'icons/icon-close.svg')
-        overlay.classList.add('over-ativo')
+
+    if (menuEstaAberto()){
+
+        fecharMenu()
     }else{
-        menu.setAttribute('src', 'icons/icon-menu.svg')
-        overlay.classList.remove('over-ativo')
-        
+
+        abrirMenu()
     }
+
+})
+//Fecha o menu pela teclha "ESC"
+document.addEventListener('keydown', (e) =>{
+    if (e.key === 'Escape' && dropdown.classList.contains('menu-aberto')) {
+        fecharMenu()
+        menu.focus()
+    }    
 })
 
+function abrirMenu() {
+    dropdown.classList.add('menu-aberto')
+    
+    menu.setAttribute('src', 'icons/icon-close.svg')
+    overlay.classList.add('over-ativo')
+    menu.setAttribute('aria-expanded', 'true')
+    menu.setAttribute('aria-label', 'opened menu')
+}
+
+function fecharMenu() {
+    dropdown.classList.remove('menu-aberto')
+    
+    menu.setAttribute('src', 'icons/icon-menu.svg')
+    overlay.classList.remove('over-ativo')
+    menu.setAttribute('aria-expanded', 'false')
+    menu.setAttribute('aria-label', 'closed menu')
+}
+
+function menuEstaAberto() {
+    return dropdown.classList.contains('menu-aberto')
+}
