87d66333d69f4a6d45517fcf0c45c448a300eab5
MarioSilva <calebemp33@gmail.com>
2026-09-20 20:46:33 -0300

    Menu com focusStrap completo

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let menu = document.getElementById('menu')
 let dropdown = document.querySelector('div.menu-fechado')
 let overlay = document.querySelector('.overlay')
+let linksFocaveis = dropdown.querySelectorAll('a')
+let primeiroElemento = linksFocaveis[0]
+let ultimoElemento = linksFocaveis[linksFocaveis.length - 1]
 // Abre e fecha o menu
 menu.addEventListener('click', (e) =>{
 
@@ -18,8 +21,23 @@ document.addEventListener('keydown', (e) =>{
     if (e.key === 'Escape' && dropdown.classList.contains('menu-aberto')) {
         fecharMenu()
         menu.focus()
-    }    
+    }
+    
+    if (e.key === 'Tab' && !e.shiftKey ) {
+        if (document.activeElement === ultimoElemento) {
+            e.preventDefault()
+            primeiroElemento.focus()
+        }        
+    }
+    if(e.key === 'Tab' && e.shiftKey){
+        if (document.activeElement === primeiroElemento) {
+            e.preventDefault()
+            ultimoElemento.focus()
+        }
+    } 
 })
+
+
 
 function abrirMenu() {
     dropdown.classList.add('menu-aberto')
@@ -42,3 +60,7 @@ function fecharMenu() {
 function menuEstaAberto() {
     return dropdown.classList.contains('menu-aberto')
 }
+
+[...linksFocaveis].forEach((link, indice) => {
+  console.log(indice, link.textContent.trim(), link)
+})
